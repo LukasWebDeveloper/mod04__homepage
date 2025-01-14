09de6c2384c0516ef3d5b2738187260a29685c94
Lukasz Duszak <lukas94.webdeveloper@gmail.com>
2025-01-14 20:31:39 +0100

    Changed, add two funtion

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-const logoElement = document.querySelector(".js-header__logo");
-const buttonLogoElement = document.querySelector(".js-button__logo");
+{
+   
+   let onChangeLogoPossition = () => {
+      const logoElement = document.querySelector(".js-header__logo");
+      logoElement.classList.toggle("js-header__blockLogo--possition");
+   }
+   
+   
+   const init = () => {
+      const buttonLogoElement = document.querySelector(".js-button__logo");
+      buttonLogoElement.addEventListener("click", onChangeLogoPossition);
+   };
 
-buttonLogoElement.addEventListener("click", () => {
-    logoElement.classList.toggle("js-header__blockLogo--possition");
-});
+   init();
+}
